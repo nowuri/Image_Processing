5bc52ac5a1daf350265327838dbdb5d58ceadac9
nowuri <109161102+nowuri@users.noreply.github.com>
2023-06-21 19:55:15 +0900

    Add files via upload

---
+++ 실습/230508/CDialog1.cpp
@@ -0,0 +1,36 @@
+﻿// CDialog1.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "MFCApplication3.h"
+#include "afxdialogex.h"
+#include "CDialog1.h"
+
+
+// CDialog1 대화 상자
+
+IMPLEMENT_DYNAMIC(CDialog1, CDialogEx)
+
+CDialog1::CDialog1(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+	, n_input1(0)
+{
+
+}
+
+CDialog1::~CDialog1()
+{
+}
+
+void CDialog1::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_input1, n_input1);
+}
+
+
+BEGIN_MESSAGE_MAP(CDialog1, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDialog1 메시지 처리기
---
+++ 실습/230508/MFCApplication3View.cpp
@@ -0,0 +1,191 @@
+﻿
+// MFCApplication3View.cpp: CMFCApplication3View 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "MFCApplication3.h"
+#endif
+
+#include "MFCApplication3Doc.h"
+#include "MFCApplication3View.h"
+#include "CDib.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication3View
+
+IMPLEMENT_DYNCREATE(CMFCApplication3View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplication3View, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CMFCApplication3View::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+	ON_COMMAND(ID_ImageInverse, &CMFCApplication3View::OnImageInverse)
+	ON_COMMAND(ID_group, &CMFCApplication3View::OnGroup)
+END_MESSAGE_MAP()
+
+// CMFCApplication3View 생성/소멸
+
+CMFCApplication3View::CMFCApplication3View() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CMFCApplication3View::~CMFCApplication3View()
+{
+}
+
+BOOL CMFCApplication3View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCApplication3View 그리기
+
+void CMFCApplication3View::OnDraw(CDC* pDC)
+{
+	CMFCApplication3Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	/*if (pDoc->m_Dib.IsValid()) {
+		pDoc->m_Dib.Draw(pDC->m_hDC);
+	}*/
+
+	//위에 것을 사용해도 되지만, 버퍼를 정확히 지정해준 것
+	if (pDoc->m_Dib.IsValid()) {
+		int w = pDoc->m_Dib.GetWidth();
+		int h = pDoc->m_Dib.GetHeight();
+		pDoc->m_Dib.Draw(pDC->m_hDC, 0, 0, w, h);
+	}
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CMFCApplication3View 인쇄
+
+
+void CMFCApplication3View::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CMFCApplication3View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFCApplication3View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CMFCApplication3View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void CMFCApplication3View::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMFCApplication3View::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMFCApplication3View 진단
+
+#ifdef _DEBUG
+void CMFCApplication3View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplication3View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplication3Doc* CMFCApplication3View::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplication3Doc)));
+	return (CMFCApplication3Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplication3View 메시지 처리기
+
+
+void CMFCApplication3View::OnInitialUpdate()
+{
+	//CView::OnInitialUpdate();
+
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	CScrollView::OnInitialUpdate();
+
+	CSize sizeTotal;
+	CMFCApplication3Doc* pDoc = GetDocument();
+	if (pDoc->m_Dib.IsValid()) {
+		sizeTotal.cx = pDoc->m_Dib.GetWidth();
+		sizeTotal.cy = pDoc->m_Dib.GetHeight();
+	}
+	else {
+		sizeTotal.cx = sizeTotal.cy = 100;
+	}
+
+	SetScrollSizes(MM_TEXT, sizeTotal);
+	ResizeParentToFit(TRUE);
+
+}
+
+//영상반전
+void CMFCApplication3View::OnImageInverse()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	CMFCApplication3Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	
+	CDib dib = pDoc->m_Dib;
+
+	pDoc->DibInverse(dib);
+	AfxNewImage(dib);
+}
+
+
+//사용자 점처리 그룹
+void CMFCApplication3View::OnGroup()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	CMFCApplication3Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+
+	CDib dib = pDoc->m_Dib;
+	pDoc->Group(dib);
+	AfxNewImage(dib);
+}
